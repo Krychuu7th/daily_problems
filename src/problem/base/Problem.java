ca33a313595aa15fdb476c33c4f49da45783f569
Krychuu7th <krychuu7th@gmail.com>
2023-01-20 18:11:46 +0100

    Daily coding problem #1

---
+++ src/problem/base/Problem.java
@@ -0,0 +1,13 @@
+package problem.base;
+
+public interface Problem {
+    String getDescription();
+    String solution();
+    default String bonusSolution() {
+        return "No bonus solution";
+    }
+
+    default void show() {
+        System.out.println(this.getDescription() + "\n" + this.solution() + "\n" + this.bonusSolution());
+    }
+}
