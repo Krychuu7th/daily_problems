f8d315bfc49699fd27cc99397f36ff84328129a2
Krychuu7th <krychuu7th@gmail.com>
2023-01-23 19:11:30 +0100

    Daily coding problem #2

--- src/DailyProblemsApp.java
+++ src/DailyProblemsApp.java
@@ -1,9 +1,19 @@
 import problem.Problem1;
+import problem.Problem2;
+import problem.problem3.Problem3;
 
 public class DailyProblemsApp {
     public static void main(String[] args) {
-        int[] problem1Array = {10, 15, 3, 7};
-        int problem1K = 17;
+        /*Problem #1*/
+        int[] problem1Array = {2, 16, 1, 7};
+        int problem1K = 9;
         new Problem1(problem1Array, problem1K).show();
+
+        /*Problem #2*/
+        int[] problem2InputArray = {1, 2, 3, 4, 5};
+        new Problem2(problem2InputArray).show();
+
+        /*Problem #3*/
+        new Problem3().show();
     }
 }
--- src/problem/Problem2.java
+++ src/problem/Problem2.java
@@ -2,18 +2,47 @@ package problem;
 
 import problem.base.Problem;
 
+import java.util.Arrays;
+
 public class Problem2 implements Problem {
     protected String description = """
-            Problem #2 [Hard]
+            Problem #2 [Hard] ------------------------------------------------------------------------------
             
-            Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
-            For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].    
+            Given an array of integers, return a new array such that each element at index i of the new array
+            is the product of all the numbers in the original array except the one at i.
+            For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24].
+            If our input was [3, 2, 1], the expected output would be [2, 3, 6].
             Follow-up: what if you can't use division?
             """;
 
+    private final int[] array;
+
+    public Problem2(int[] array) {
+        this.array = array;
+    }
+
     @Override
     public String solution() {
-        return null;
+        if (this.array.length <= 1) {
+            return "No solution for given params";
+        }
+
+        int[] outputArray = new int[this.array.length];
+        for (int i = 0; i < this.array.length; i++) {
+            Integer iResult = null;
+            for (int j = 0; j < this.array.length; j++) {
+                if (j != i) {
+                 if (iResult == null) {
+                     iResult = this.array[j];
+                 } else {
+                     iResult *= this.array[j];
+                 }
+                }
+            }
+            outputArray[i] = iResult;
+        }
+
+        return "Input array: " + Arrays.toString(this.array) + "\nOutput array: " + Arrays.toString(outputArray);
     }
 
     @Override
